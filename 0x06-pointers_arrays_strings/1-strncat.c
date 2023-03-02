2401876848cda3b0642ead879cf6be5da0fd297a
Jacqueline-malak <jacquelinemalak230@gmail.com>
2023-03-02 15:33:21 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates n bytes of two strings
+ * @dest: pinter destination 
+ * @src: pointer source
+ * @n: number of bytes
+ * Return: void
+ * */
+
+char *_strncat(char *dest, char *src, int n)
+{int dest_len, i;
+	for(dest_len = 0; dest[dest_len] != '\0'; dest_len++);
+       for(i = 0; i < n && src[i] != '\0'; i++)
+	       dest[dset_len + i] = src[i];
+       /*should end with a end of string char*/
+       dest[dest_len + i] = '\0';
+       return(dest);
+}
